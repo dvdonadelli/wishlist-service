a2fb866c636c7711cdd46183cd57aca37fcf0d5d
donadelli <davi_donadelli@live.com>
2023-04-25 09:45:12 -0300

    feat: initial commit

---
+++ src/test/java/com/dvdonadelli/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvdonadelli.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
